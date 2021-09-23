ece34d1b896ba12317f153d56f1876c7ecc66f40
aande <aande@DESKTOP-ILVEVUH>
2021-09-23 23:43:50 +0300

    Add project files.

---
+++ TestTaskCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskCode
+{
+    class PizzasStructure
+    {
+        [JsonProperty("toppings")]
+        public List<string> Toppings { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(Console.ReadLine());
+
+            var list = JsonConvert.DeserializeObject<List<PizzasStructure>>(json);
+            foreach (var result in list.GroupBy(z => string.Join(",", z.Toppings)).OrderByDescending(x => x.Count()).Take(20))
+            {
+                Console.WriteLine($"{result.Key}: {result.Count()}");
+            }
+
+        }
+    }
+}
